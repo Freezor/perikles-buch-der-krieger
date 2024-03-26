1c3abac0383f366448db33b2ced18ec625a3b371
Oliver <info@oliver-fries.de>
2024-03-26 21:18:22 +0100

    update content

--- scripts/setup.js
+++ scripts/setup.js
@@ -10,15 +10,12 @@ function initializeWeaponProperties() {
     CONFIG.DND5E.weaponProperties['gunpowder'] = 'Schießpulver';
     CONFIG.DND5E.weaponProperties['nonFatal'] = 'Nichttödlich';
     CONFIG.DND5E.weaponProperties['parry'] = 'Parieren';
-    CONFIG.DND5E.weaponProperties['penetrating'] = 'Durchdringend';
     CONFIG.DND5E.weaponProperties['pierce'] = 'Durchbohren';
-    CONFIG.DND5E.weaponProperties['range'] = 'Reichweite';
     CONFIG.DND5E.weaponProperties['recharge'] = 'Aufladen';
     CONFIG.DND5E.weaponProperties['splintering'] = 'Zersplitternd';
     CONFIG.DND5E.weaponProperties['stunning'] = 'Betäubend';
     CONFIG.DND5E.weaponProperties['swinging'] = 'Schwungvoll';
     CONFIG.DND5E.weaponProperties['thrown'] = 'Geworfen';
-    CONFIG.DND5E.weaponProperties['variable'] = 'Variabel';
     CONFIG.DND5E.weaponProperties['winged'] = 'Geflügelt';
 }
 
@@ -32,11 +29,56 @@ function initializeAmmunitionProperties() {
 }
 
 function initializeArmorProperties() {
-    CONFIG.DND5E.armorProperties['armorShit'] = 'Armor Shit';
+    CONFIG.DND5E.shieldIds.buckler = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.xwlTqcf1oExvM4LR';
+    CONFIG.DND5E.shieldIds.tripleshield = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.mDY1koxDIuuPPbIT';
+    CONFIG.DND5E.shieldIds.duellshield = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.YAfFieIjrHoyyXwA';
+    CONFIG.DND5E.shieldIds.normanicshield = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.awsBsP9BgvEJ6cRG';
+    CONFIG.DND5E.shieldIds.pavese = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.vtH4lF5YdVdnmJab';
+    CONFIG.DND5E.shieldIds.roundshield = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.FZRnaJO6dzSAw3VP';
+    CONFIG.DND5E.shieldIds.towershield = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.6pgSUH4744tRXaMX';
+    CONFIG.DND5E.armorIds.brigandine = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.CM4bq5zM4q2Vonlt';
+    CONFIG.DND5E.armorIds.gambeson = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.5qrcHXPMsrqIGoKz';
+    CONFIG.DND5E.armorIds.halfplate = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.HfX34h24YAyCHoEK';
+    CONFIG.DND5E.armorIds.halsberge = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.Dww4tbuWr9aPDXhk';
+    CONFIG.DND5E.armorIds.cuirass = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.fAgQ0RFsi9c8uJRO';
+    CONFIG.DND5E.armorIds.fullplate = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.W1PLxco8s0OTmo8M';
 }
 
 function initializeBaseWeaponIds() {
-    CONFIG.DND5E.weaponIds ['armorShit'] = 'Armor Shit';
+    CONFIG.DND5E.weaponIds.almarada = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.IrttjuCbj4jpPXLu';
+    CONFIG.DND5E.weaponIds.arcebuse = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.YJc3yvw3jvzXhmkq';
+    CONFIG.DND5E.weaponIds.atlatl = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.nAHfMkvBzTEAEP2D';
+    CONFIG.DND5E.weaponIds.bastardsword = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.igaYzaUqYyM9Fdwo';
+    CONFIG.DND5E.weaponIds.farmerflail = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.DRjBPH8JtdrECEdx';
+    CONFIG.DND5E.weaponIds.boomerang = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.P61ifrCCTMIn8mls';
+    CONFIG.DND5E.weaponIds.cestus = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.6Mng8h2EVqypLYar';
+    CONFIG.DND5E.weaponIds.boarspear = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.hI7ALM0DdNBM7kJP';
+    CONFIG.DND5E.weaponIds.estoc = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.ntVLzLJfWQDj9wdv';
+    CONFIG.DND5E.weaponIds.garottewire = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.XPcDQkAKTgOH1xo9';
+    CONFIG.DND5E.weaponIds.gladius = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.Kx1PRDkLc6ST0bbV';
+    CONFIG.DND5E.weaponIds.goedendag = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.f4hCdUR5e3RFKzlz';
+    CONFIG.DND5E.weaponIds.greatbow = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.zoAoJVlHfZtLq6ZC';
+    CONFIG.DND5E.weaponIds.greatclub = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.nedhliZ1WjoxIqzJ';
+    CONFIG.DND5E.weaponIds.guisarme = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.MSbTej9XVSH69obj';
+    CONFIG.DND5E.weaponIds.hookblade = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.FTzJZKc4hjnTZvU1';
+    CONFIG.DND5E.weaponIds.handcanon = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.ObFphUxgVoSpkutF';
+    CONFIG.DND5E.weaponIds.harpoone = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.N8f16SHxbdhmQXSV';
+    CONFIG.DND5E.weaponIds.huntingbow = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.NKv2d13fHJum5z2m';
+    CONFIG.DND5E.weaponIds.huntingknife = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.9Qa0QGVNYxUanI2I';
+    CONFIG.DND5E.weaponIds.battleknife = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.eBrho5A7D9TZg1kt';
+    CONFIG.DND5E.weaponIds.katana = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.3hu85KacrIyEJxWb';
+    CONFIG.DND5E.weaponIds.chainwhip = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.tlUjYnh5ZqOJL7X6';
+    CONFIG.DND5E.weaponIds.battlesword = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.osA3nHCTUpBFmMaw';
+    CONFIG.DND5E.weaponIds.shortspear = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.7kWMcv5alaKt1C0H';
+    CONFIG.DND5E.weaponIds.lucernehammer = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.Qm3rxEzefNiSW9aQ';
+    CONFIG.DND5E.weaponIds.swordglove = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.vwk7z1DWbsKNSAfI';
+    CONFIG.DND5E.weaponIds.parryingdagger = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.GstavWdT6UA0CrBV';
+    CONFIG.DND5E.weaponIds.ranseur = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.HDLJaSHfDflBzvWp';
+    CONFIG.DND5E.weaponIds.reflexbow = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.pZ4t4FJaIFFVxphf';
+    CONFIG.DND5E.weaponIds.rondel = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.QTpjpx4eM5bMGiK8';
+    CONFIG.DND5E.weaponIds.rodslinger = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.aVsJqmtNaqBaqfqk';
+    CONFIG.DND5E.weaponIds.shuriken = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.fcqYydST8xXNrca0';
+    CONFIG.DND5E.weaponIds.battleaxeskewer = 'Compendium.perikles-buch-der-krieger.pbk-items.Item.LevwEG0ov0M56kXP';
 }
 
 function initializeConsumablesTypes() {
@@ -45,11 +87,11 @@ function initializeConsumablesTypes() {
     CONFIG.DND5E.consumableTypes['bomb'] = 'Bombe';
 }
 
-function initializeConsumptionTypes(){
+function initializeConsumptionTypes() {
     CONFIG.DND5E.abilityConsumptionTypes['fighterEndurance'] = 'Kämpfer Ausdauer';
 }
 
-function initializeWeaponFeatureType(){
+function initializeWeaponFeatureType() {
     CONFIG.DND5E.featureTypes['periklesWeapons'] = {
         label: "Perikles Waffenmerkmale",
         subtypes: {
@@ -65,13 +107,11 @@ Hooks.on('init', () => {
     initializeAmmunitionProperties();
     initializeWeaponFeatureType();
     initializeConsumptionTypes();
+    initializeBaseWeaponIds();
+    initializeArmorProperties();
 });
 
 Hooks.on('ready', () => {
     initializeWeaponProperties();
-});
-
-Hooks.on('renderItemSheet', (app, html, data) => {
-
 });
 
